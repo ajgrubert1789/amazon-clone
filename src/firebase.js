ff734b8a04ad82d66e742bd78ee12aad7bd3dd6a
ajgrubert1789 <64898101+ajgrubert1789@users.noreply.github.com>
2024-02-07 08:17:01 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVwOJ_HwK-ghEkx772vOJ70_CmNo6RGrw",
-  authDomain: "challenge-e653e.firebaseapp.com",
-  projectId: "challenge-e653e",
-  storageBucket: "challenge-e653e.appspot.com",
-  messagingSenderId: "36197392859",
-  appId: "1:36197392859:web:8012a9330858e8b90f87fe",
-  measurementId: "G-XQB48FYKT1",
+  
 };
 
 // Initialize Firebase
